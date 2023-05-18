12210ff5d0d65f2a7ca57aaccfe9ec6f98c1e6d1
velpre <veljko_kv@hotmail.com>
2023-05-18 12:37:04 +0200

    connected collection view done

---
+++ Shortcut.oppgave/Controllers/ListComicsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Shortcut.oppgave
+//
+//  Created by VP on 18/05/2023.
+//
+
+import UIKit
+
+class ListComicsViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        print("test")
+    }
+
+
+}
+
+extension ListComicsViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) 
+        return cell
+    }
+}
+
+
+
+
+
